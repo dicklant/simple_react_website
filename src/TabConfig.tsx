b60e2aff9bacbb967f7e0c4735b3fe4a188652b7
dicklant <dicklant@hotmail.com>
2022-10-25 22:18:19 +0200

    Comment teams

--- src/TabConfig.tsx
+++ src/TabConfig.tsx
@@ -22,11 +22,12 @@ class TabConfig extends React.Component {
             this.tabName = event.target.value;
         }
 
+ /*
         microsoftTeams.app.initialize();
 
         microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
             // eslint-disable-next-line no-template-curly-in-string
-            const baseUrl = "https://app.getyellow.io"; 
+            const baseUrl = "https://simple-react-teams.azurewebsites.net/"; 
             //const routeBase = "/tab";
 
             microsoftTeams.pages.config.setConfig({
@@ -38,7 +39,7 @@ class TabConfig extends React.Component {
 
             saveEvent.notifySuccess();
         });
-
+*/
         return <div>
             <h1>Hello config</h1>
             <p>Nom de l'onglet</p>
