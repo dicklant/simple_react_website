30594bb3909d75196b1fc7f547402a37b8303407
dicklant <dicklant@hotmail.com>
2022-10-25 23:03:06 +0200

    MS Teams Initialize v2

--- src/TabConfig.tsx
+++ src/TabConfig.tsx
@@ -7,9 +7,9 @@ interface IProps {
 
 class TabConfig extends React.Component {
 
-    tabName : string;
-    
-    constructor (props : IProps) {
+    tabName: string;
+
+    constructor(props: IProps) {
         super(props);
 
         this.tabName = "Yellow tab";
@@ -22,24 +22,58 @@ class TabConfig extends React.Component {
             this.tabName = event.target.value;
         }
 
-        microsoftTeams.app.initialize();
+        // Initialize the Microsoft Teams SDK
+        app.initialize().then(() => {
+            /**
+             * When the user clicks "Save", save the url for your configured tab.
+             * This allows for the addition of query string parameters based on
+             * the settings selected by the user.
+             */
+            pages.config.registerOnSaveHandler((saveEvent) => {
+                const baseUrl = `https://simple-react-teams.azurewebsites.net`;
 
-        /*
-        microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
-            // eslint-disable-next-line no-template-curly-in-string
-            const baseUrl = "https://simple-react-teams.azurewebsites.net/"; 
-            //const routeBase = "/tab";
-
-            microsoftTeams.pages.config.setConfig({
-                suggestedDisplayName : this.tabName,
-                entityId : "Test",
-                contentUrl : baseUrl,
-                websiteUrl : baseUrl,
+                pages.config.setConfig({
+                    suggestedDisplayName: this.tabName,
+                    entityId: "Test",
+                    contentUrl: baseUrl,
+                    websiteUrl: baseUrl,
+                }).then(() => {
+                    saveEvent.notifySuccess();
+                });
             });
 
-            saveEvent.notifySuccess();
+            /**
+             * After verifying that the settings for your tab are correctly
+             * filled in by the user you need to set the state of the dialog
+             * to be valid.  This will enable the save button in the configuration
+             * dialog.
+             */
+            pages.config.setValidityState(true);
         });
-*/
+
+
+
+
+
+
+        /*
+                microsoftTeams.app.initialize();
+        
+                microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
+                    // eslint-disable-next-line no-template-curly-in-string
+                    const baseUrl = "https://simple-react-teams.azurewebsites.net/"; 
+                    //const routeBase = "/tab";
+        
+                    microsoftTeams.pages.config.setConfig({
+                        suggestedDisplayName : this.tabName,
+                        entityId : "Test",
+                        contentUrl : baseUrl,
+                        websiteUrl : baseUrl,
+                    });
+        
+                    saveEvent.notifySuccess();
+                });
+        */
         return <div>
             <h1>Hello config</h1>
             <p>Nom de l'onglet</p>
